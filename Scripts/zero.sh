a846f9df6a73ae134a28f09c5c01f4c4b0b067d4
ALbur <24468956+ALbur@users.noreply.github.com>
2023-05-14 22:09:33 +0800

    Update zero.sh

--- Scripts/zero.sh
+++ Scripts/zero.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-PHY_IFACE=ifconfig | grep  "Link" | awk '{print $1}'|grep "et"
-ZT_IFACE=ifconfig | grep  "Link" | awk '{print $1}'|grep "zt"
+PHY_IFACE=$(ifconfig | grep "Link" | awk '{print $1}' | grep "eth" | head -n1)
+ZT_IFACE=$(ifconfig | grep "Link" | awk '{print $1}' | grep "zt" | head -n1)
 iptables -t nat -A POSTROUTING -o $PHY_IFACE -j MASQUERADE
 iptables -A FORWARD -i $PHY_IFACE -o $ZT_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $ZT_IFACE -o $PHY_IFACE -j ACCEPT
-iptables-save
+#iptables-save
